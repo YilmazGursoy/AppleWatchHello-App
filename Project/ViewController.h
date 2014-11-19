31b9245c6f3353f5a6255fd1383ddce7f989240e
YilmazGursoy <ylmzgrsy111@gmail.com>
2014-11-19 12:49:09 +0200

    Initial Commit

---
+++ Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project
+//
+//  Created by MostWanted on 19/11/14.
+//  Copyright (c) 2014 yilmaz gursoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Project/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Project
+//
+//  Created by MostWanted on 19/11/14.
+//  Copyright (c) 2014 yilmaz gursoy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
