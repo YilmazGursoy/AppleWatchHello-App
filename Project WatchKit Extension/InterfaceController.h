a0ea950e2f242756cf1e75e02811c17c00fae019
YilmazGursoy <ylmzgrsy111@gmail.com>
2014-11-23 03:41:19 +0200

    AppleWatch WatchKit SDK Hello World App

---
+++ Project WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  Project WatchKit Extension
+//
+//  Created by MostWanted on 20/11/14.
+//  Copyright (c) 2014 yilmaz gursoy. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ Project WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  Project WatchKit Extension
+//
+//  Created by MostWanted on 20/11/14.
+//  Copyright (c) 2014 yilmaz gursoy. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
